56be5383120a01e6c9f7b39e951954f9b5609021
robotto <mark@moore.dk>
2016-01-18 22:29:56 +0100

    debounce all the things

--- Arduino/sketch.ino
+++ Arduino/sketch.ino
@@ -9,7 +9,7 @@ Debounce can be commented in/out if (un)needed
 #define MAX_ACTIVATION_TIME 30000UL //miliseconds
 
 const int inputPin = 2;
-const int outputPin = 13;
+const int outputPin = 12;
 unsigned long timeAtActivation=0;
 
 void setup() {
@@ -24,6 +24,7 @@ void loop() {
 	timeAtActivation=millis();
 	digitalWrite(outputPin,HIGH); //activate
 	while(digitalRead(inputPin) && millis()<timeAtActivation+MAX_ACTIVATION_TIME); //while high && while MAX_ACTIVATION_TIME is not exceeded
+	//delay(20); //debounce .. probably not needed
 	digitalWrite(outputPin,LOW); //deactivate
 	while(digitalRead(inputPin)); //wait for pin to go low... (only relevant if timer was maxed out)
 	//delay(20); //debounce .. probably not needed
