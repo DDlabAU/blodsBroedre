7efdf26f7461292707a645495cb45ee75af399aa
robotto <mark@moore.dk>
2016-01-18 12:45:27 +0100

    more comments, more documentation

--- Arduino/sketch.ino
+++ Arduino/sketch.ino
@@ -1,5 +1,12 @@
+/*
+This sketch monitors and relays the state of inputPin to outputPin
+A the high state can be maintained for a maximum of MAX_ACTIVATION_TIME.
+Debounce can be commented in/out if (un)needed
+*/
 
-#define MAX_ACTIVATION_TIME 30000 //miliseconds
+
+
+#define MAX_ACTIVATION_TIME 30000UL //miliseconds
 
 const int inputPin = 2;
 const int outputPin = 13;
@@ -12,12 +19,12 @@ void setup() {
 }
 
 void loop() {
-
 	while(!digitalRead()); //while low.
-	delay(20); //debounce .. probably not needed
+	//delay(20); //debounce ..
 	timeAtActivation=millis();
 	digitalWrite(outputPin,HIGH); //activate
 	while(digitalRead() && millis()<timeAtActivation+MAX_ACTIVATION_TIME); //while high && while MAX_ACTIVATION_TIME is not exceeded
 	digitalWrite(outputPin,LOW); //deactivate
-
+	while(digitalRead()); //wait for pin to go low... (only relevant if timer was maxed out)
+	//delay(20); //debounce .. probably not needed
 }
